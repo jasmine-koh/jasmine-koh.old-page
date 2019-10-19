396e298c1fadca98dde3e48e3c948e683faac44c
jasmine-koh <jazs0515@gmail.com>
2019-10-19 10:13:09 +0800

    complete gameplay logic

--- GA_Project_1/app.js
+++ GA_Project_1/app.js
@@ -128,6 +128,22 @@ let gameCards = [
 
 let characterArray = [];
 let playerArray = [];
+let humanArray = [];
+let superHumanArray = [];
+let werewolfArray = [];
+
+
+// Shuffling the Array
+const shuffle = function() {
+    for (let i = characterArray.length-1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i)
+        const temp = characterArray[i];
+        characterArray[i] = characterArray[j];
+        characterArray[j] = temp;
+    }
+}
+
+
 
 // Number of Players
 const numOfPlayers = function() {
@@ -163,16 +179,19 @@ const createCharacters = function() {
             alert(`You have created ${characterArray.length} characters, it is now good to go.`);
         }
         // console.log(characterArray);
+        shuffle();
 }
 
 
-// Shuffling the Array
-const shuffle = function() {
-    for (let i = characterArray.length-1; i > 0; i--) {
-        const j = Math.floor(Math.random() * i)
-        const temp = characterArray[i];
-        characterArray[i] = characterArray[j];
-        characterArray[j] = temp;
+
+// function to find the card image based on the type of character
+const arrayPosition = function(array, input) {
+
+    for (let i=0; i< array.length; i++) {
+        // console.log(gameCards[i].character);
+        if (array[i].character === input) {
+            return array[i];
+        };
     }
 }
 
@@ -188,6 +207,7 @@ const createCard = function() {
 
         // assigning attributes to the element
         let cardElement = $(`<img>`).attr(`src`,`images/card-back.jpg`).attr(`id`,cardProperty).appendTo($(`#cards`));
+
         $(cardElement).on(`click`,flipCard);
 
     }
@@ -198,21 +218,19 @@ const flipCard = function() {
     // getting the type of character
     let characterId = this.getAttribute(`id`);
 
-    // function to find the card image based on the type of character
-    const arrayPosition = function() {
-        for (let i=0; i< gameCards.length; i++) {
-            // console.log(gameCards[i].character);
-            if (gameCards[i].character === characterId) {
-                return gameCards[i];
-            };
-        }
-    }
-
     // to display the character card
-    this.setAttribute(`src`, arrayPosition().cardImage);
+    this.setAttribute(`src`, arrayPosition(gameCards, characterId).cardImage);
 
     // push character into player array to store player's character
-    playerArray.push(arrayPosition());
+    playerArray.push(arrayPosition(gameCards, characterId));
+
+    if (characterId === `witch` || characterId === `seer`) {
+        superHumanArray.push(arrayPosition(gameCards, characterId));
+    } else if (characterId === `werewolf`) {
+        werewolfArray.push(arrayPosition(gameCards, characterId));
+    } else if (characterId === `villager`) {
+        humanArray.push(arrayPosition(gameCards, characterId));
+    }
 
     setTimeout( () => {
         this.remove();
@@ -226,25 +244,18 @@ const flipCard = function() {
 // Creating divs for two columns required for game play
 const players = function() {
 
+    console.log(playerArray);
+    console.log(superHumanArray);
+    console.log(werewolfArray);
+    console.log(humanArray);
+
         for (let i=0; i< playerArray.length; i++) {
 
             // to get the type of character for card image
             let cardProperty = Object.getOwnPropertyDescriptor(playerArray[i], 'character').value;
 
-                const arrayPosition = function() {
-                    for (let i=0; i< gameCards.length; i++) {
-
-                        // console.log(gameCards[i].character);
-                        if (gameCards[i].character === cardProperty) {
-                            return gameCards[i];
-                        };
-
-                    }
-                }
-
-
             // assigning attributes to the element
-            $(`<img>`).attr(`src`, arrayPosition().cardImage).attr(`id`,cardProperty).addClass(`char`).appendTo($(`#displayPlayerCharacter`));
+            $(`<img>`).attr(`src`, arrayPosition(gameCards, cardProperty).cardImage).attr(`id`,cardProperty).addClass(`char`).appendTo($(`#displayPlayerCharacter`));
 
         }
     }
@@ -257,138 +268,149 @@ const deathImage = (event) => {
 
             let cardId = $(event.currentTarget).attr(`id`);
 
-            const arrayPosition = function() {
-                for (let i=0; i< gameCards.length; i++) {
-
-                    // console.log(gameCards[i].character);
-                    if (gameCards[i].character === cardId) {
-                        return gameCards[i];
-                    };
-                }
-            }
         // assigning attributes to the element
-        $(event.currentTarget).removeClass(`dead`).attr(`src`, arrayPosition().cardImage);
+        $(event.currentTarget).removeClass(`dead`).attr(`src`, arrayPosition(gameCards, cardId).cardImage);
 
     } else {
 
             let cardId = $(event.currentTarget).attr(`id`);
 
-            const arrayPosition = function() {
-                for (let i=0; i< gameCards.length; i++) {
-
-                    // console.log(gameCards[i].character);
-                    if (gameCards[i].character === cardId) {
-                        return gameCards[i];
-                    };
-                }
-            }
         // assigning attributes to the element
-        $(event.currentTarget).addClass(`dead`).attr(`src`, arrayPosition().deadImage);
+        $(event.currentTarget).addClass(`dead`).attr(`src`, arrayPosition(gameCards, cardId).deadImage);
     }
 
 }
 
 
+const speechTime = function() {
 
+    $(`#nextButton7`).remove();
 
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton8`).appendTo($(`#modDiv`));
+    // let end = $(`<button>`).text(`End`).addClass(`btn btn-primary`).attr(`id`,`end`).appendTo($(`#modDiv`));
 
+    // console.log(`day time`);
 
-const dayTime = function() {
+    // $(`.char`).on(`click`, deathImage);
+    $(`#moderator`).html(`Mod, announce players that were killed. Starting from first living player, players give their speech. <br/> After introduction, vote to kill a player. <br/> Please click on the player that is killed. <br/> Moderator, please check and announce if game has ened.`);
+    $(`#nextButton8`).on(`click`, nightTime);
+    // $(`#end`).on(`click`,)
 
-    $(`#moderator`).text(`<Announce the players that were killed. The players should not reveal their character.>`);
-    $(`#nextButton`).on(`click`, () => {
-        $(`#moderator`).text(`<Starting from the first living player, players start introducing themselves.>`);
-        $(`#nextButton`).on(`click`, () => {
-            $(`#moderator`).text(`<After introduction, players will need to vote to kill a player who they think is a wolf.>`);
-            $(`#nextButton`).on(`click`, () => {
-                $(`#moderator`).text(`<Please click on the player that is killed.>`);
-                $(`.char`).on(`click`, deathImage);
-                $(`#nextButton`).on(`click`, nightTime);
-            })
-        })
-    })
+    // console.log(`day to night time`);
 }
-
 
 
 const wolfTime = function() {
 
-    $(`#moderator`).text(`Werewolves, please open your eyes.`);
-    $(`#nextButton`).on(`click`, () => {
-        $(`#moderator`).text(`Werewolves, please pick someone to kill.`);
-        $(`#nextButton`).on(`click`, () => {
-            $(`#moderator`).text(`<Please click on the player that is killed.>`);
-            $(`.char`).on(`click`, deathImage);
-            $(`#nextButton`).on(`click`, () => {
-                $(`#moderator`).text(`Werewolves, please close your eyes.`);
-                $(`#nextButton`).on(`click`, witchTime)
-            })
-        })
-    })
+    $(`#nextButton2`).remove();
+
+    // console.log(`wolf time`);
+
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton3`).appendTo($(`#modDiv`));
+
+    // $(`.char`).on(`click`, deathImage);
+    $(`#moderator`).html(`Werewolves, please open your eyes. Pick someone to kill. <br/> Mod, please click on the player that is killed. <br/> Werewolves, please close your eyes.`);
+    $(`#nextButton3`).on(`click`, witchPotionTime);
+
+    // console.log(`wolf to potion time`);
+}
+
+
+const witchPotionTime = function() {
+
+    $(`#nextButton3`).remove();
+
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton4`).appendTo($(`#modDiv`));
+
+    // console.log(`potion time`);
+    // $(`.char`).on(`click`, deathImage);
+    $(`#moderator`).html(`Witch, please open your eyes. <br/> This player has been killed. Would you like to save the player? <br/> If the witch decides to save the player, please click on the player.`);
+
+    $(`#nextButton4`).on(`click`, witchPoisonTime);
+
+    // console.log(`potion to poison time`);
+
 
 }
 
 
-const witchTime = function() {
+const witchPoisonTime = function() {
 
-    $(`#moderator`).text(`Witch, please open your eyes.`);
-    $(`#nextButton`).on(`click`, () => {
-        $(`#moderator`).text(`This player has been killed. Would you like to save the player?`);
-        $(`#nextButton`).on(`click`, () => {
-            $(`#moderator`).text(`<If the witch decides to save the player, please click on the player.>`);
-            $(`.char dead`).on(`click`, deathImage);
-            $(`#nextButton`).on(`click`, () => {
-                $(`#moderator`).text(`Would you like to use your poison?`);
-                $(`#nextButton`).on(`click`, () => {
-                    $(`#moderator`).text(`<If the witch decides to use the poison, please click on the player.>`);
-                    $(`.char`).on(`click`, deathImage);
-                    $(`#nextButton`).on(`click`, () => {
-                        $(`#moderator`).text(`Witch, please close your eyes.`);
-                        $(`#nextButton`).on(`click`, seerTime);
-                    })
-                })
-            })
-        })
-    })
+    $(`#nextButton4`).remove();
+
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton5`).appendTo($(`#modDiv`));
+
+    // console.log(`poison time`);
+
+    // $(`.char`).on(`click`, deathImage);
+    $(`#moderator`).html(`Would you like to use your poison? </br> If the witch decides to use the poison, please click on the player. </br> Witch, please close your eyes.`);
+    $(`#nextButton5`).on(`click`, seerTime);
+
+    // console.log(`poison to seer time`);
 
 }
 
 
 const seerTime = function() {
 
-    $(`#moderator`).text(`Seer, please open your eyes.`);
-    $(`#nextButton`).on(`click`, () => {
-        $(`#moderator`).text(`Seer, please pick someone to ask about.`);
-        $(`#nextButton`).on(`click`, () => {
-            $(`#moderator`).text(`<If the person is a werewolf, show a thumbs down. Otherwise, show a thumbs up.>`);
-            $(`#nextButton`).on(`click`, () => {
-                $(`#moderator`).text(`Seer, please close your eyes.`);
-                $(`#nextButton`).on(`click`, () => {
-                    $(`#moderator`).text(`Open your eyes, it is now day time.`);
-                    $(`#nextButton`).on(`click`, dayTime);
-                })
-            })
-        })
-    })
+    $(`#nextButton5`).remove();
 
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton6`).appendTo($(`#modDiv`));
+
+    // console.log(`seer time`);
+    $(`#moderator`).html(`Seer, please open your eyes. Please pick someone to ask about. <br/> If the person is a werewolf, show a thumbs down. Otherwise, show a thumbs up. <br/> Seer, please close your eyes.`);
+    $(`#nextButton6`).on(`click`, dayTime);
+
+    // console.log(`seer to day time`);
 }
 
+
+const dayTime = function() {
+
+    $(`#nextButton6`).remove();
+
+    day ++;
+
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton7`).appendTo($(`#modDiv`));
+
+    $(`#moderator`).html(`Open your eyes, it is now day time.`);
+    $(`#nextButton7`).on(`click`, speechTime);
+}
 
 
 // night time script for Moderator
 const nightTime = function() {
 
-        $(`#moderator`).text(`Close your eyes, it is now night time.`);
-        $(`#nextButton`).on(`click`, wolfTime)
+    $(`button`).remove();
+
+    night ++;
+
+   let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton2`).appendTo($(`#modDiv`));
+
+    // console.log(`night time`)
+    $(`#moderator`).html(`Close your eyes, it is now night time.`);
+    $(`#nextButton2`).on(`click`, wolfTime);
+
+    // console.log(`night to wolf time`);
 
 }
 
 
-const nextButton = function() {
+const removeButton = function() {
 
-        let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton`).attr(`float`,`right`).appendTo($(`#modDiv`));
+    $(`#nextButton0`).remove();
+    $(`.char`).on(`click`, deathImage);
+    nightTime();
+}
+
+const gameStart = function() {
+
+    let next = $(`<button>`).text(`Next`).addClass(`btn btn-primary`).attr(`id`,`nextButton0`).appendTo($(`#modDiv`));
+
+    $(`#nextButton0`).on(`click`, removeButton);
 
 }
+
 
 
 
@@ -404,15 +426,18 @@ let werewolvesNum = 0;
 let villagersNum = 0;
 let seersNum = 0;
 let witchesNum = 0;
+let night = 0;
+let day = 0;
+let arrayCount = 0;
 
-
-
-
-
-
-// how to win and who wins
-    // if wolf kills all human or all supernatural character, evil wins
-    // if all wolves are killed, good wins
+let timeArray = [
+    function() {nightTime()},
+    function() {wolfTime()},
+    function() {witchPotionTime()},
+    function() {witchPoisonTime()},
+    function() {seerTime()},
+    function() {dayTime()}
+]
 
 
 
@@ -425,14 +450,12 @@ $(() => {
     $(`#characterAllocation`).on(`click`, createCharacters);
 
 // Setting Up The Game
-    $(`#start`).on(`click`, shuffle);
     $(`#start`).on(`click`, createCard);
 
 // Create layout for Game
     $(`#continue`).on(`click`, players);
-    // $(`#continue`).on(`click`, generateProps);
-    $(`#continue`).on(`click`, nextButton);
-    $(`#continue`).on(`click`, nightTime);
 
+// Start Game
+    $(`#continue`).on(`click`, gameStart);
 
 })
